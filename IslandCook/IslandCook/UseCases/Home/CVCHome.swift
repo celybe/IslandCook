882463941b28e6632772be80320d7c87e2f91da1
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-02-13 19:40:40 +0000

    Trying to implement a horizontal tableView

---
+++ IslandCook/IslandCook/UseCases/Home/CVCHome.swift
@@ -0,0 +1,14 @@
+//
+//  CVCHome.swift
+//  IslandCook
+//
+//  Created by user211717 on 2/13/22.
+//
+
+import UIKit
+
+class CVCHome: UICollectionViewCell {
+    @IBOutlet weak var txtTitle: UILabel!
+    @IBOutlet weak var ivRecipe: UIImageView!
+    
+}
---
+++ IslandCook/IslandCook/UseCases/Home/VCHome.swift
@@ -0,0 +1,52 @@
+//
+//  VCHome.swift
+//  IslandCook
+//
+//  Created by user211717 on 2/13/22.
+//
+
+import UIKit
+
+class VCHome: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func clickAll(_ sender: Any) {
+    }
+    @IBAction func clickPasta(_ sender: Any) {
+    }
+    @IBAction func clickDesssert(_ sender: Any) {
+    }
+    @IBAction func clickDinner(_ sender: Any) {
+    }
+    @IBAction func clickEasy(_ sender: Any) {
+    }
+    @IBAction func clickMedium(_ sender: Any) {
+    }
+    @IBAction func clickHigh(_ sender: Any) {
+    }
+    
+}
