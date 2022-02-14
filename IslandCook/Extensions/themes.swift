4f300077986e2bf71a4a71239956b300135e59b3
Aracely <79402005+aracelyberrocalhuerta@users.noreply.github.com>
2022-02-14 20:42:37 +0100

    Themes do it!

---
+++ IslandCook/Extensions/themes.swift
@@ -0,0 +1,17 @@
+//
+//  themes.swift
+//  IslandCook
+//
+//  Created by user209640 on 2/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let background = Color("BackgroundColor")
+}
