8c127369419df590d3709974751dc4af8eb43e15
Xavi Girón <xavier.giron@live.u-tad.com>
2022-02-17 18:02:19 +0100

    Finished alerts

---
+++ IslandCook/IslandCook/Provider/API/Model/Alerts/AlertPushRecipes/ACPushRecipes.swift
@@ -0,0 +1,24 @@
+//
+//  ACPushRecipes.swift
+//  IslandCook
+//
+//  Created by Xavi Giron on 17/2/22.
+//
+
+import UIKit
+
+class ACPushRecipes: UIAlertController {
+
+    var msgError = "Whoopss, something didn't came out the way it was supposed to"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alert = UIAlertController(title: "Push error", message: msgError, preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
