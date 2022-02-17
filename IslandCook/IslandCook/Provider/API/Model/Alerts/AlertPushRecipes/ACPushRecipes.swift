4d3fc8d691b52e11f1ead31ec1a1917c2d3520da
Xavi Girón <xavier.giron@live.u-tad.com>
2022-02-17 18:56:38 +0100

    update

--- IslandCook/IslandCook/Provider/API/Model/Alerts/AlertPushRecipes/ACPushRecipes.swift
+++ IslandCook/IslandCook/Provider/API/Model/Alerts/AlertPushRecipes/ACPushRecipes.swift
@@ -14,7 +14,6 @@ class ACPushRecipes: UIAlertController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
