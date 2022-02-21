80f6573bc8aee3d478c2deab66482de0dc312f4a
Aracely <79402005+aracelyberrocalhuerta@users.noreply.github.com>
2022-02-21 10:43:12 +0100

    trying to do favourites view

--- IslandCook/IslandCook/UseCases/Favorites/VCFavorites.swift
+++ IslandCook/IslandCook/UseCases/Favorites/VCFavorites.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class VCFavorites: UIViewController {
+class VCFavorites: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource   {
+    
+    let myRecipesExamples : [String] = ["https://cdn.vox-cdn.com/thumbor/DQneqacgrmet47pxQDwvDqhBYSI=/0x0:900x1125/920x613/filters:focal(478x728:622x872):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/68862911/La_Comida_tacos.0.jpg%22,%22https://cdn.vox-cdn.com/thumbor/r6lodJrk_CHIO101QRLWwwAJ_EM=/0x0:1600x800/920x613/filters:focal(672x272:928x528):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/63656524/Comida.0.png%22,%22https://cdn.aarp.net/content/dam/aarp/health/caregiving/2018/03/1140-nutrients-food-loved-ones-caregiving-esp.imgcache.rev.web.700.403.jpg"]
 
+    let myRecipesLabels : [String] = ["Receta 1","Receta 2","Receta 3"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myRecipesExamples.count<#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        if let recipeCell = dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CVCellFavorites {
+            recipeCell.configure(recipeLabel: myRecipesLabels[indexPath.row], recipeImage: myRecipesExamples[indexPath.row])
+        }
+        return cell
+    }
+    
+
+    @IBOutlet weak var collectionView: CVFavorites!
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
 
