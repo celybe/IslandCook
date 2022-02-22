ae815fe8e72ea814dfeea98d9fdb8aabb3c5bb81
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-02-22 18:38:26 +0000

    Added parameters in save recipe in DB

--- IslandCook/IslandCook/Provider/DataBase/CoreDataManager.swift
+++ IslandCook/IslandCook/Provider/DataBase/CoreDataManager.swift
@@ -19,10 +19,19 @@ class CoreDataManager{
         return container
     }()
     
-    func saveContext(){
+    func saveContext(id: String, author: String, difficulty: String,  ingredients: String, name: String, picture_url: String, steps: Steps, ingredients_recipe: Ingredients, tags_recipe: Tags){
         let context = persistentContainer.viewContext
         if context.hasChanges{
             do{
+                let recipe = NSEntityDescription.insertNewObject(forEntityName: "Recipe", into: context) as! Recipe
+                recipe.id = id
+                recipe.author=author
+                recipe.difficulty = difficulty
+                recipe.picture_url = picture_url
+                recipe.ingredients_recipe = ingredients_recipe
+                recipe.name = name
+                recipe.steps_recipe = steps
+                recipe.tags_recipe = tags_recipe
                 try context.save()
             }catch{
                 print("Save in database failed,\(error as NSError)")
